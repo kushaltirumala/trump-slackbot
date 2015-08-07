21e1446de0f39a3fbdf2536fbd85c763a717ba84
Ethan Lee <ethanlee@ethansites.com>
2015-08-06 23:30:53 -0700

    Separating stuff out, quote additions

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+/*
+ *	Made by Ethan Lee (ethanlee16) and Kushal Tirumala (kushaltimurala)
+ *  Licensed under the MIT License.
+ */
+
 /* Change this to your Slack bot's OAuth token, 
 * found in the Integrations tab */
-var SLACK_TOKEN = "";
+var SLACK_TOKEN = require('./config').slackToken;
 
 var https = require('https');
 var  _ws = require('ws');
+var r = require('./responses');
+
 var counter = 1;
-var ws;
+var ws, slackID;
 
 https.get("https://slack.com/api/rtm.start?token=" + SLACK_TOKEN, function(res) {
 	console.log("Connecting to Slack API...");
@@ -18,6 +25,7 @@ https.get("https://slack.com/api/rtm.start?token=" + SLACK_TOKEN, function(res) 
 	}).on('end', function() {
 		var rtm = JSON.parse(data);
 		ws = new _ws(rtm.url);
+		slackId = rtm.self.id;
 		console.log("Logging into " + rtm.team.name + "'s Slack...");
 		ws.on('open', function() {
 			goTrump(rtm.team.name, rtm.team.prefs.default_channels[0]);
@@ -38,7 +46,9 @@ function goTrump(teamName, channelID) {
 	console.log("Listening for new messages...");
 	ws.on('message', function(data) {
 		var event = JSON.parse(data);
-		if(event.type === "message") {
+		// debug
+		// console.log(JSON.stringify(event));
+		if(event.type === "message" && event.user !== slackID) {
 			ws.send(JSON.stringify({
 				"id": counter,
 				"type": "message",
@@ -50,41 +60,11 @@ function goTrump(teamName, channelID) {
 	});
 }
 
-var r = [
-	{
-		"keywords": ["america", "usa", "united states"],
-		"messages": [
-			"When was the last time anybody saw us beating, let’s say, " 
-			+ "China in a trade deal? They kill us. I beat China all the time."
-		]
-	},
-	{
-		"keywords": ["mexico", "mexican", "latino"],
-		"messages": [
-			"When Mexico sends its people, they’re not sending their best. " 
-			+ "They’re sending people that have lots of problems.",
-			"I will build a great wall — and nobody builds walls better than me, " 
-			+ "believe me —and I’ll build them very inexpensively. I will build, " 
-			+ "a great, great wall on our southern border, and I will make Mexico " 
-			+ "pay for that wall. Mark my words."
-		]
-	},
-	{
-		"keywords": ["china", "asia", "japan", "korea"],
-		"messages": [
-			"When did we beat Japan at anything? They send their cars over " 
-			+ "here by the millions, and what do we do? When was the last time " 
-			+ "you saw a Chevrolet in Tokyo? It doesn't exist, folks."
-		]
-	}
-];
-
 function getResponse(message) {
-	console.log("looking for " + message);
 	for(var i = 0; i < r.length; i++) {
 		for(var j = 0; j < r[i].keywords.length; j++) {
 			if(message.toLowerCase().indexOf(r[i].keywords[j]) != -1) {
-				console.log("true");
+				console.log("Responding to message: " + message);
 				return r[i].messages[Math.floor(Math.random() * r[i].messages.length)];
 			}
 		}
